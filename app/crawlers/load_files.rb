d132f7ad787d05a44d3a3e4d5502ef073aadb31a
Brennan Novak <bnvk@transparencytoolkit.org>
2016-05-31 14:22:43 +0200

    updated DirCrawl to use local Tika config
    TransparencyToolkit/EmailParser#8

--- app/crawlers/load_files.rb
+++ app/crawlers/load_files.rb
@@ -1,13 +1,16 @@
 class LoadFiles
   def initialize(dir)
     @dir = dir
+	# TODO: need a method to pass "tika" value from a global/environement var
+	@tika = nil
   end
 
   # Load in docs
   def run
+
     # Make blocks for dircrawl
-    block = lambda do |file, in_dir, out_dir|
-      p = ParseFile.new(file, in_dir, out_dir)
+    block = lambda do |file, in_dir, out_dir, tika|
+      p = ParseFile.new(file, in_dir, out_dir, tika)
       p.parse_file
     end
 
@@ -15,10 +18,14 @@ class LoadFiles
       require 'parsefile'
     end
 
-    # Call dircrawl
+    # Output Directory
     out_dir = @dir+"_output"
-    d = DirCrawl.new(@dir, out_dir, "_terms", block, include, @dir, out_dir)
+
+	# Extras
+	extras = lambda do |out_dir|
+	end
+
+    d = DirCrawl.new(@dir, out_dir, "_terms", false, block, include, extras, @dir, out_dir, @tika)
     JSON.parse(d.get_output)
   end
 end
-
