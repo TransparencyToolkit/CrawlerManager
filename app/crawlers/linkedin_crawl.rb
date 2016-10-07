d9e362e3e0cacc47b8ae79bff8a36a167b61dbe4
shidash <shidash@shidash.com>
2016-10-07 18:27:19 -0400

    Updated to use LI gem

--- app/crawlers/linkedin_crawl.rb
+++ app/crawlers/linkedin_crawl.rb
@@ -1,15 +1,26 @@
 class LinkedinCrawl
-  def initialize(search_query)
+  def initialize(harvester_url, selector_id, search_query)
     @search_query = search_query
+    @harvester_url = harvester_url
+    @selector_id = selector_id
   end
 
   # Crawl LinkedIn
   def run
-    requests_linkedin = RequestManager.new(ENV['PROXYLIST'], [1, 3], 5) 
-    requests_google = RequestManager.new(ENV['PROXYLIST'], [1, 3], 1)
-    captcha_settings = ENV['SOLVERDETAILS'] != nil ? {captcha_key: ENV['SOLVERDETAILS']} : nil
-    c = LinkedinCrawler.new(@search_query, 1, requests_linkedin, requests_google, captcha_settings)
-    c.search
-    JSON.parse(c.gen_json)
+    Thread.new do
+      # Setup incremental result passing hash
+      cm_hash = {crawler_manager_url: @harvester_url,
+               selector_id: @selector_id}
+
+      # Setup request assing info
+      requests_linkedin = RequestManager.new(ENV['PROXYLIST'], [1, 3], 1) 
+      requests_google = RequestManager.new(ENV['PROXYLIST'], [1, 3], 1)
+      requests_google2 = RequestManager.new(ENV['PROXYLIST'], [1, 3], 1)
+      captcha_settings = ENV['SOLVERDETAILS'] != nil ? {captcha_key: ENV['SOLVERDETAILS']} : nil
+
+      # Scrape
+      c = LinkedinCrawler.new(@search_query, 1, requests_linkedin, requests_google, requests_google2, captcha_settings, cm_hash)
+      c.search
+    end
   end
 end
