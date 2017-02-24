8fd4fe63730f4332c6cbf1b23ccf1e0e5fc7ea41
Brennan Novak <bnvk@transparencytoolkit.org>
2017-02-25 00:45:46 +0100

    use icon-twitter now

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ crawlers = [
      :date_retrieved,
      :page_title], { primary_id_field: "url",
                      secondary_id_field: ["page_title"]}],
-  ["Twitter", "icon-google", "Crawls Twitter search", {search_query: "string", search_operators: "string"}, "TwitterCrawl", [
+  ["Twitter", "icon-twitter", "Crawls Twitter search", {search_query: "string", search_operators: "string"}, "TwitterCrawl", [
      :tweet_text,
      :username,
      :fullname,
